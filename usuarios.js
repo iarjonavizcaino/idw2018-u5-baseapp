462a1ad9016665f8707566fc7f1104fbe3191886
Israel Arjona <iarjona@ittepic.edu.mx>
2018-07-25 11:33:53 -0600

    CRUD completo

--- usuarios.js
+++ usuarios.js
@@ -1,3 +1,61 @@
+$(function () {
+    $('#tbUsuarios').DataTable({
+        language: {
+            //url:"http://cdn.datatables.net/plug-ins/1.10.12/i18n/Spanish.json"
+            "decimal": "",
+            "emptyTable": "No hay información",
+            "info": "Mostrando _START_ to _END_ of _TOTAL_",
+            "infoEmpty": "No ha datos",
+            "infoFiltered": "(filtered from _MAX_ total entries)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_",
+            "loadingRecords": "Cargando...",
+            "processing": "Procesando...",
+            "search": "Buscar:",
+            "zeroRecords": "No se encontró",
+            "paginate": {
+                "first": "<<",
+                "last": ">>",
+                "next": ">",
+                "previous": "<"
+            },
+            "aria": {
+                "sortAscending": ": activate to sort column ascending",
+                "sortDescending": ": activate to sort column descending"
+            }
+        },
+        responsive:true,
+        ajax: {
+            url: "http://localhost:3002/usuarios/v1/usuario/",
+            dataSrc: function (datos) {
+                console.log(datos.users);
+                return datos.users;
+            }
+        },
+        columns: [
+            {
+                data: "name"
+            },
+            {
+                data: "email"
+            },
+            {
+                data: function (row) {
+                    console.log(row._id);
+                    var res = `<button id="btnBorrar"
+                                class="btn btn-danger btn-xs" 
+                                onclick="borrar('${row._id}')">
+                                Eliminar
+                                </button>`;
+                    return res;
+                }
+            }
+        ]
+    });
+});
+
+
 function guardar() {
     //var name = document.getElementById("txtNombre").value;
     var name = $('#txtNombre').val();
@@ -11,23 +69,47 @@ function guardar() {
 
     $.ajax(
         {
-            url:"http://localhost:3002/usuarios/v1/usuario/",
-            type:"POST",
+            url: "http://localhost:3002/usuarios/v1/usuario/",
+            type: "POST",
             data: {
-                name:name,
-                email:email,
-                password:password
+                name: name,
+                email: email,
+                password: password
             }
         }
     )
-    .done(
-        function(data) {
-            alert(JSON.stringify(data));
-        }
-    )
-    .fail(
-        function(err) {
-            alert(err);
-        }
-    );
+        .done(
+            function (data) {
+                alert(JSON.stringify(data));
+
+                $('#txtNombre').val('');
+                $('#txtEmail').val('');
+                $('#txtPassword').val('');
+
+                $('#tbUsuarios').dataTable().api().ajax.reload();
+            }
+        )
+        .fail(
+            function (err) {
+                alert(err);
+            }
+        );
+}
+
+function borrar(id) {
+    $.ajax({
+        url: "http://localhost:3002/usuarios/v1/usuario/" + id,
+        type: "delete"
+    })
+        .done(
+            function (data) {
+                alert(data.msg);
+                $('#tbUsuarios').dataTable().api().ajax.reload();
+            }
+        )
+        .fail(
+            function (err) {
+                alert(err);
+            }
+        )
 }
